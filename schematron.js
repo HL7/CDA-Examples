4327e3c4d219da28216dae4cbd6efba93eb39ac7
Benjamin Flessner <b.d.flessner@gmail.com>
2025-07-07 10:11:29 -0500

    Add 3.0 Schematron

--- schematron.js
+++ schematron.js
@@ -13,28 +13,84 @@ const skippedAssertions = [
   'a-1098-30784-v'  // This is a warning which the schematron validator treats as an error
 ];
 
-const filenameFilter = process.argv[2];
-const schematron = fs.readFileSync('ccda-2-1.sch', 'utf8');
-const header = `
-   _____       _____ _____            ___   __               
-  / ____|     / ____|  __ \\   /\\     |__ \\ /_ |              
- | |   ______| |    | |  | | /  \\       ) | | |              
- | |  |______| |    | |  | |/ /\\ \\     / /  | |              
- | |____     | |____| |__| / ____ \\   / /_ _| |              
-  \\_____|     ______|_____/_/    \\_\\ |____(_)_|              
-  / ____|    | |                        | |                  
- | (___   ___| |__   ___ _ __ ___   __ _| |_ _ __ ___  _ __  
-  \\___ \\ / __| '_ \\ / _ \\ '_ \` _ \\ / _\` | __| '__/ _ \\| '_ \\ 
-  ____) | (__| | | |  __/ | | | | | (_| | |_| | | (_) | | | |
- |_____/ \\___|_| |_|\\___|_| |_| |_|\\__,_|\\__|_|  \\___/|_| |_|
-`;
+const schematronData = {
+  '2.1': {
+    fileName: 'ccda-2-1.sch',
+    contents: null,
+    parsedMap: null,
+    header: `
+ ██████╗    ██╗
+ ╚════██╗  ███║
+  █████╔╝  ╚██║
+ ██╔═══╝    ██║
+ ███████╗██╗██║
+ ╚══════╝╚═╝╚═╝`
+  },
+  '3.0': {
+    fileName: 'ccda-3-0.sch',
+    contents: null,
+    parsedMap: null,
+    header: `
+██████╗     ██████╗ 
+╚════██╗   ██╔═████╗
+ █████╔╝   ██║██╔██║
+ ╚═══██╗   ████╔╝██║
+██████╔╝██╗╚██████╔╝
+╚═════╝ ╚═╝ ╚═════╝`
+  }
+};
+
+const filenameFilter = Object.keys(schematronData).includes(process.argv[2]) ? process.argv[3] : process.argv[2] || null;
+
+// TextToASCII Font ANSI Shadow
+const headerBase = `
+ ██████╗        ██████╗██████╗  █████╗    
+██╔════╝       ██╔════╝██╔══██╗██╔══██╗   
+██║     █████╗ ██║     ██║  ██║███████║   
+██║     ╚════╝ ██║     ██║  ██║██╔══██║   
+╚██████╗       ╚██████╗██████╔╝██║  ██║   
+ ╚═════╝        ╚═════╝╚═════╝ ╚═╝  ╚═╝   
+      
+███████╗ ██████╗██╗  ██╗███████╗███╗   ███╗ █████╗ ████████╗██████╗  ██████╗ ███╗   ██╗
+██╔════╝██╔════╝██║  ██║██╔════╝████╗ ████║██╔══██╗╚══██╔══╝██╔══██╗██╔═══██╗████╗  ██║
+███████╗██║     ███████║█████╗  ██╔████╔██║███████║   ██║   ██████╔╝██║   ██║██╔██╗ ██║
+╚════██║██║     ██╔══██║██╔══╝  ██║╚██╔╝██║██╔══██║   ██║   ██╔══██╗██║   ██║██║╚██╗██║
+███████║╚██████╗██║  ██║███████╗██║ ╚═╝ ██║██║  ██║   ██║   ██║  ██║╚██████╔╝██║ ╚████║
+╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝`;
+
+const headerDash = `
+        
+        
+ █████╗ 
+ ╚════╝ `;
+
+const header40 = `
+██╗  ██╗    ██████╗ 
+██║  ██║   ██╔═████╗
+███████║   ██║██╔██║
+╚════██║   ████╔╝██║
+     ██║██╗╚██████╔╝
+     ╚═╝╚═╝ ╚═════╝`;
+
+
+const schematron = process.argv[2] === '3.0' ? schematronData['3.0'] : schematronData['2.1'];
+
+schematron.contents = fs.readFileSync(schematron.fileName, 'utf8');
+if (!schematron.contents) {
+  console.error(`Error: Schematron file ${schematron.fileName} not found. Please ensure it is in the current directory.`);
+  process.exit(1);
+}
+
+// TODO - add dash and range when we do more simultaneously!
+const header = headerBase.split('\n').map((base, index) => base + (schematron.header.split('\n')[index] || '')).join('\n');
 
 console.log(header);
 console.log(filenameFilter ? `Validating example files matching: ${filenameFilter}` : 'Validating all sample files (pass in a string to filter files)');
 
-if (!schematron) {
-  console.error('Error: Schematron file not found. Please ensure ccda-2-1.sch is in the current directory.');
-  process.exit(1);
+// Quick fix since cda-schematron-validator doesn't handle variables, and I can't work on that right now
+const matches = Array.from(schematron.contents.matchAll(/<let name="([\w_]+)" value="('[^'"]+')"\/>/g));
+for (const variable of matches) {
+  schematron.contents = schematron.contents.replaceAll(`($${variable[1]},`, `(${variable[2]},`);
 }
 
 const inputDir = path.join(__dirname, 'input', 'examples');
@@ -43,7 +99,6 @@ if (!fs.existsSync(inputDir)) {
   process.exit(1);
 }
 
-let schematronMap;
 const files = fs.readdirSync(inputDir);
 const xmlFiles = files.filter(file => file.endsWith('.xml') && (!filenameFilter || file.includes(filenameFilter)));
 xmlFiles.forEach(validateFile);
@@ -60,7 +115,7 @@ process.stdin.setRawMode(true);
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (key) => {
-  if (key === 'q') {
+  if (key === 'q' || key === '\u0003') { // 'q' or Ctrl+C
     console.log('Stopping watch⌚ mode and exiting...');
     watcher.close();
     process.exit(0);
@@ -74,10 +129,10 @@ function validateFile(file) {
 
     validator.clearCache();
     let options = {
-      parsedSchematronMap: schematronMap
+      parsedSchematronMap: schematron.parsedMap
     };
-    const results = validator.validate(data, schematron, options);
-    schematronMap = options.schematronMap; // Cache the schematron map for future use
+    const results = validator.validate(data, schematron.contents, options);
+    schematron.parsedMap = options.schematronMap; // Cache the schematron map for future use
     results.ignored = results.ignored.filter(i => !knownIgnoredTests.find(search => i.test.includes(search)));
     results.ignored.forEach(i => {
       console.warn(`Ignored test: ${i.test} in file ${file}`);
